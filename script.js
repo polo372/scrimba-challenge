f7cfe53ae2d86bc79da4d5b28dde621b3220e6a5
polo372 <plbd@laposte.net>
2025-09-24 16:08:26 +0200

    retour du fetch + affiche de l'ensemble des films

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const containerPrincipal = document.getElementById("container-principal")
 const searchBtn = document.getElementById("search")
-searchBtn.addEventListener("click",() => {
-    fetch("http://www.omdbapi.com/?apikey=f01736b6&t=blade")
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-    containerPrincipal.innerHTML = `<p> ${data.Titlte} ${data.imdbRating} </p>
-    <img src="${data.Poster}"/>
-    <button>add me to the localstorage</button>`
-    })
+const movieSearch = document.getElementById("movie-search")
+
+searchBtn.addEventListener("click", () => {
+    // vide le container avant une nouvelle recherche
+    containerPrincipal.innerHTML = ""
+    let value = movieSearch.value
+    fetch(`http://www.omdbapi.com/?apikey=f01736b6&s=${value}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.Response === "True") {
+                for (let i = 0; i < data.Search.length; i++) {
+                    let imdbId = data.Search[i].imdbID
+                    fetch(`http://www.omdbapi.com/?apikey=f01736b6&i=${imdbId}`)
+                        .then(res => res.json())
+                        .then(dataMovie => {
+                            console.log(dataMovie)
+                            containerPrincipal.innerHTML += 
+                            `<p>${dataMovie.Title} ${dataMovie.imdbRating}</p>
+                             <p>${dataMovie.Runtime} ${dataMovie.Genre}</p>
+                             <button>add me to the localstorage</button>
+                             <p>${dataMovie.Plot}</p>
+                             <img src="${dataMovie.Poster}"/>`
+                        })
+                }
+            } else {
+                containerPrincipal.innerHTML = 
+                    `<p>Unable to find what you’re looking for. Please try another search</p>`
+            }
+        })
 })
